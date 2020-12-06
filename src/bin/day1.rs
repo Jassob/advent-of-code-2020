7536f2b6bdd27025aa598176f1f248a952315201
Jacob Jonsson <Jassob@users.noreply.github.com>
2020-12-06 17:58:43 +0100

    chore(day1): use new scaffolding

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,58 +1,37 @@
-use std::fs;
-
 fn main() -> Result<(), String> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 4 {
-        Err("Usage: day1 --part <1|2> <input>")?
-    }
-    let part = &args[2];
-    let input_file = &args[3];
-    let content = fs::read_to_string(input_file)
-        .or_else(|err| Err(format!("failed to read input: {}", err)))
-        .and_then(parse_input)?;
-    match if part == "1" {
-        part1(content)
-    } else {
-        part2(content)
-    } {
-        Some(val) => println!("Result: {}", val),
-        None => Err("could not find any bad input")?,
-    }
+    let (part, content) = utils::parse_args()?;
+    let input = content
+        .lines()
+        .map(|l| {
+            l.parse::<u32>()
+                .map_err(|e| format!("failed to parse number: {}", e))
+        })
+        .collect::<Result<Vec<u32>, String>>()?;
+    utils::run(part1, part2, part, input);
 
     Ok(())
 }
 
-fn parse_input(input: String) -> Result<Vec<u32>, String> {
-    input.lines().try_fold(vec![], |mut acc, line| {
-        line.parse::<u32>()
-            .and_then(|val| {
-                acc.push(val);
-                Ok(acc)
-            })
-            .or_else(|err| Err(format!("failed to parse number: {}", err)))
-    })
-}
-
-fn part1(numbers: Vec<u32>) -> Option<u32> {
+fn part1(numbers: Vec<u32>) -> u32 {
     for i in numbers.iter() {
         for j in numbers.iter() {
             if i + j == 2020 {
-                return Some(i * j);
+                return i * j;
             }
         }
     }
-    None
+    panic!("found no such number pair");
 }
 
-fn part2(numbers: Vec<u32>) -> Option<u32> {
+fn part2(numbers: Vec<u32>) -> u32 {
     for i in numbers.iter() {
         for j in numbers.iter() {
             for k in numbers.iter() {
                 if i + j + k == 2020 {
-                    return Some(i * j * k);
+                    return i * j * k;
                 }
             }
         }
     }
-    None
+    panic!("found no such number triple");
 }
